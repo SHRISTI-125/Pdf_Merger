63b8496e61668670d610dbc56c534ca91413d712
Shristi <136554443+SHRISTI-125@users.noreply.github.com>
2024-01-30 23:06:01 +0530

    code.py

---
+++ code.py
@@ -0,0 +1,10 @@
+import PyPDF2
+
+
+pdf=['Supervised_Learning.pdf','Unsupervised Learning.pdf']
+combo = PyPDF2.PdfFileMerger()
+
+for i in pdf:
+    combo.append(i)
+combo.write("2Concepts.pdf")
+combo.close()
